292f9702943a1a751e0adf8b1ce6aad1adb9bc66
Federico Huneeus <fahuneeu@uc.cl>
2022-03-27 20:56:54 -0300

    Adding first commit

---
+++ .slicemachine/assets/slices/TextBlock/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/TextBlock';
+
+export default {
+  title: 'slices/TextBlock'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"text_block","items":[],"primary":{"title":[{"type":"heading1","text":"Brand B2B architectures","spans":[]}],"description":[{"type":"paragraph","text":"Nulla aliquip nulla fugiat enim culpa deserunt adipisicing esse aute ut sint do occaecat sunt duis. Laborum ea amet dolor fugiat aute amet.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
